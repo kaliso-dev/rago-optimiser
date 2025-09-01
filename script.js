fe81a733716bbd89eeb4ef383f21f8a75e1a4b9d
kaliso-dev <sosollamsouni@gmail.com>
2025-09-01 11:01:02 +0200

    check up monday

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks   = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  const isOpen = navLinks.classList.toggle("active");
+  menuToggle.setAttribute("aria-expanded", isOpen ? "true" : "false");
+});

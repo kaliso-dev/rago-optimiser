6899391f53725c730597286fb515b265e1fdec66
kaliso-dev <sosollamsouni@gmail.com>
2025-09-01 11:45:47 +0200

    menu toggle animation and size

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const navLinks   = document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
   const isOpen = navLinks.classList.toggle("active");
+  menuToggle.classList.toggle("active"); // ajoute/retire le X
   menuToggle.setAttribute("aria-expanded", isOpen ? "true" : "false");
 });

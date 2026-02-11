a98a0f86f9bb56e9a83787ee3912dde88e51c903
Kaliso Dev <141718414+kaliso-dev@users.noreply.github.com>
2026-02-11 16:21:41 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const navLinks   = document.querySelector(".nav-links");
-
-menuToggle.addEventListener("click", () => {
-  const isOpen = navLinks.classList.toggle("active");
-  menuToggle.classList.toggle("active"); // ajoute/retire le X
-  menuToggle.setAttribute("aria-expanded", isOpen ? "true" : "false");
-});
